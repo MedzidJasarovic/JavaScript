78abdf6349777ff6551f0c29e93d5848864955c7
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2021-12-23 13:39:22 +0100

    js comit

---
+++ app.js
@@ -0,0 +1,74 @@
+/* broj1 = prompt("Unesi prvi broj,trazi daris ");
+broj2 = prompt("Unesi drugi broj, trazi daris ");
+broj3 = parseInt(broj1) + parseInt(broj2);
+console.log(broj3);   
+
+
+
+ /*  prompt("Ovo sad ovde ce da ti racuna broj dana u broju godina");
+brojgodina=prompt("Unesite broj godina")
+brojmeseci=parseInt(brojgodina)*365;
+brojdana=parseInt(brojgodina)*365;
+console.log("U",brojgodina,"godina ima",brojmeseci,"meseci i",brojdana,"dana");
+/* domakkiiii odgeldaj tuki
+
+*/
+//----------------------------------------------------------------------------------
+/*
+
+let niz = [Imena,Prezimena];     //deklaracija funkcije i niza
+const funkcija = (niz) => {
+  for (i; i < niz.lenght; i++) {
+    if (i != 4) {
+      console.log(i);
+    }
+  }
+};
+
+*/
+
+/*
+let Imena = { ime: "Medzid", ime2: "Belma" }; //kreiranje objekta 
+let Prezimena = { prezime: "Jasarovic", prezime2: "Maljanovic" };
+
+let slova = [a, b, c, d, e, f, g];
+let rezultat = slova.length; //dobijanje duzine niza
+let odabranoslovo=slova[5]//pristupanje clana niza po indeksu
+
+let imeee = Imena.ime; // uzimanje vrednosti kljuca iz objekta
+*/
+
+// 2 cas ------------------------------------------------------------------------------
+
+/*null  - rezervisano mesto u memoriji bez brednosti
+ = uzima vrednost
+ == iste vrednosti
+ === iste vrednosti i tio (strick equal)
+
+
+  false || false === false
+  false || true === true
+  true  || false === true
+  true  || true === true
+
+
+  false && false === false
+  true && false === false
+  false && true === false
+  true && true === true
+
+  
+
+broj1 = prompt("Unesi prvi broj");
+broj2 = prompt("Unesi drugi broj");
+console.log(parseInt(broj1) < parseInt(broj2));
+
+
+b1 = prompt("Unesi broj");
+console.log(parseInt(b1) % 2 == 0);
+
+*/
+
+ime = prompt("unesite vase ime");
+duzina = ime.lenght;
+console.log(duzina);
