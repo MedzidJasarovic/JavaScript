c5a935274c50cb84999d52647d9e579062323dcd
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2022-01-21 17:47:10 +0100

    gagis

--- 8_ArrayVezbanje/app.js
+++ 8_ArrayVezbanje/app.js
@@ -6,16 +6,31 @@ Expected Output :
 [4, 5, 8, 10, 12, 13]
 */
 array1 = [1, 0, 2, 3, 4];
-array2 = [3, 5, 6, 7, 8, 13];
-summ = 0;
+array2 = [3, 5, 6, 7, 8, 13, 5, 69, 8];
+summ = [];
 
 if (array1.length > array2.length) {
   min = array2.length;
 } else {
   min = array1.length;
 }
-for (i = 0; i < min; i++) {
-  summ += array1[i];
-  summ += array2[i];
+
+if (array1.length > array2.length) {
+  max = array1.length;
+} else {
+  max = array2.length;
 }
+
+for (i = 0; i < min; i++) {
+  summ[i] = array1[i] + array2[i];
+}
+
+for (i = Math.abs(array1.length - array2.length); i < max; i++) {
+  if (array1.length > array2.length) {
+    summ[i] = array1[i];
+  } else {
+    summ[i] = array2[i];
+  }
+}
+
 console.log(summ);
