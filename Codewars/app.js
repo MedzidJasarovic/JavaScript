663b0dbecf02cf27a10765ba8ad8f2f4b9c177ec
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2022-02-10 12:21:20 +0100

    ponovoradigit

--- Codewars/app.js
+++ Codewars/app.js
@@ -475,12 +475,41 @@
 // return rezultat.to();
 
 ////////////////////////////////////////////////////////////////////MUMBLING
-s = "abcde";
-rezultat = [];
-s = s.toUpperCase().split("");
-for (i = 0; i < s.length; i++) {
-  for (j = 0; j < i + 1; j++) {
-    rezultat.push(s[i]);
-  }
+// s = "abcde";
+// rezultat = [];
+// s = s.toUpperCase().split("");
+// for (i = 0; i < s.length; i++) {
+//   for (j = 0; j < i + 1; j++) {
+//     rezultat.push(s[i]);
+//   }
+// }
+// console.log(rezultat);
+////////////////////////////////////////////////////////////// SUM STRINGS AS NUMBERS 4 kyu
+
+function sumStrings(a, b) {
+  return a >= 9007199254740992 - 1 || b >= 9007199254740992 - 1
+    ? bigInt(a, b)
+    : a * 1 + b * 1 + "";
 }
-console.log(rezultat);
+
+function bigInt(a, b) {
+  var c = "";
+  var d = 0;
+
+  var zero = new Array(Math.abs(a.length - b.length) + 1).join("0");
+
+  a.length >= b.length ? (b = zero + b) : (a = zero + a);
+
+  for (var i = b.length - 1; i >= 0; i--) {
+    var sum = a[i] * 1 + b[i] * 1 + d;
+    if (sum >= 10) {
+      sum = sum - 10;
+      d = 1;
+    } else {
+      d = 0;
+    }
+    c = sum + c;
+  }
+
+  return d > 0 ? d + c : c;
+}
