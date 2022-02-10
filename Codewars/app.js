ed6030f2f9bb2f891e33f437fb0aa32af268c326
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2022-02-10 12:39:39 +0100

    ff

--- Codewars/app.js
+++ Codewars/app.js
@@ -486,30 +486,52 @@
 // console.log(rezultat);
 ////////////////////////////////////////////////////////////// SUM STRINGS AS NUMBERS 4 kyu
 
-function sumStrings(a, b) {
-  return a >= 9007199254740992 - 1 || b >= 9007199254740992 - 1
-    ? bigInt(a, b)
-    : a * 1 + b * 1 + "";
-}
+// function sumStrings(a, b) {
+//   return a >= 9007199254740992 - 1 || b >= 9007199254740992 - 1
+//     ? bigInt(a, b)
+//     : a * 1 + b * 1 + "";
+// }
 
-function bigInt(a, b) {
-  var c = "";
-  var d = 0;
+// function bigInt(a, b) {
+//   var c = "";
+//   var d = 0;
 
-  var zero = new Array(Math.abs(a.length - b.length) + 1).join("0");
+//   var zero = new Array(Math.abs(a.length - b.length) + 1).join("0");
 
-  a.length >= b.length ? (b = zero + b) : (a = zero + a);
+//   a.length >= b.length ? (b = zero + b) : (a = zero + a);
 
-  for (var i = b.length - 1; i >= 0; i--) {
-    var sum = a[i] * 1 + b[i] * 1 + d;
-    if (sum >= 10) {
-      sum = sum - 10;
-      d = 1;
-    } else {
-      d = 0;
-    }
-    c = sum + c;
+//   for (var i = b.length - 1; i >= 0; i--) {
+//     var sum = a[i] * 1 + b[i] * 1 + d;
+//     if (sum >= 10) {
+//       sum = sum - 10;
+//       d = 1;
+//     } else {
+//       d = 0;
+//     }
+//     c = sum + c;
+//   }
+
+//   return d > 0 ? d + c : c;
+// }
+
+//////////////////////////////////////CAT AND DOG YEARS
+
+var humanYearsCatYearsDogYears = function (human) {
+  dog = 0;
+  cat = 0;
+  result = [];
+  if (human == 1) {
+    cat = 15;
+    dog = 15;
+  } else if (human == 2) {
+    cat = 24;
+    dog = 24;
+  } else if (human > 2) {
+    cat = 24 + (human - 2) * 4;
+    dog = 24 + (human - 2) * 5;
   }
-
-  return d > 0 ? d + c : c;
-}
+  result.push(human);
+  result.push(cat);
+  result.push(dog);
+  return result;
+};
