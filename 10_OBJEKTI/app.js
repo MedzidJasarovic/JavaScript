7a8579c880de4b59be6264ff098802205b07b83a
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2022-02-11 19:20:13 +0100

    cap

--- 10_OBJEKTI/app.js
+++ 10_OBJEKTI/app.js
@@ -1,19 +1,29 @@
-var library = [
-  {
-    autor: "Bill Gates",
-    readingStatus: true,
-  },
-  {
-    autor: "Steve Jobs",
-    readingStatus: true,
-  },
-  {
-    autor: "Suzanaaa",
-    readingStatus: false,
-  },
-];
-for (i = 0; i < library.length; i++) {
-  console.log(library[i].autor, library[i].readingStatus); /////////////////// ISPISE SVE TRAZENE ATRIBUTE
-}
+// var library = [
+//   {
+//     autor: "Bill Gates",
+//     readingStatus: true,
+//   },
+//   {
+//     autor: "Steve Jobs",
+//     readingStatus: true,
+//   },
+//   {
+//     autor: "Suzanaaa",
+//     readingStatus: false,
+//   },
+// ];
+// for (i = 0; i < library.length; i++) {
+//   console.log(library[i].autor, library[i].readingStatus); /////////////////// ISPISE SVE TRAZENE ATRIBUTE
+// }
 
 ///////////////////////////// OBJEKTI SU REFERENTN TIPOVI PODATAKA ///////IDI NA W3Resource
+function covek() {
+  const osoba = {};
+  osoba.ime = prompt("Unesite ime");
+  osoba.prezime = prompt("Unesite prezime");
+  osoba.godine = prompt("Unesite godine");
+  osoba.visina = prompt("Unesite visinu");
+  return osoba;
+}
+
+console.log(covek());
