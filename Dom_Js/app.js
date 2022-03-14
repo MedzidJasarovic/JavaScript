96f13bb62a2b1ab81c823a9f5eeb480dfddf050e
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2022-03-14 19:07:19 +0100

    lolita

---
+++ Dom_Js/app.js
@@ -0,0 +1,20 @@
+//
+// const el = document.getElementsByClassName("main");
+// const el = document.getElementById("heading");
+// const el = document.getElementsByName("");
+// const el = document.getElementsByTagName("h1");
+// const el = document.querySelector("h1");
+// const el = document.querySelector("h1");
+// el.style.backgroundColor = "red";
+// el.style.background = "blue";
+// el.style.fontSize = "40px";
+const noJuda = document.getElementsByClassName("ww2");
+noJuda[0].style.background = "red";
+noJuda[1].style.background = "red";
+
+const juda = document.getElementById("jevrejskih1");
+juda.style.backgroundColor = "cyan";
+juda.style.fontSize = "10px";
+
+lupin = document.getElementsByTagName("div");
+lupin.style.background = "black";
